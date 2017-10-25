36dced29930e68ca10bd5dbf221682ec6cb5a83f
monkey <gikeda@appirits.com>
2017-10-25 18:34:21 +0900

    [add] JavaでPreferencesFragmentの実装

--- app/src/main/java/com/list_sample/preferencesample/MainActivity.java
+++ app/src/main/java/com/list_sample/preferencesample/MainActivity.java
@@ -3,7 +3,8 @@ package com.list_sample.preferencesample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements PreferenceDemoFragment.OnFragmentInteractionListener {
+    private PreferenceDemoFragment.OnFragmentInteractionListener mListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
